6b7e365f0d4080fdacadb9e3c33660ac412ec3e6
fguchelaar <fguchelaar@gmail.com>
2014-12-15 00:37:36 +0100

    implemented basic checksum validation; happy-path only.

--- FGNmeaKit/FGNmeaSentence.m
+++ FGNmeaKit/FGNmeaSentence.m
@@ -32,7 +32,39 @@
         }
         return NO;
     }
+
+    if(![self validateChecksum:sentence]) {
+        if (error != NULL) {
+            *error = [[NSError alloc] initWithDomain:@"FGNmeaSentenceValidation" code:2 userInfo:nil];
+        }
+        return NO;
+    }
     return YES;
+}
+
+/**
+* A sentence can optionally contain a checksum. If it does, there is an asterisk at the end of the sentence followed
+* by a 2 character checksum (HEX). If there is no checksum available, YES will be returned, otherwise the checksum
+* will be validated.
+*/
++ (BOOL)validateChecksum:(NSString *)sentence {
+
+    NSRange asteriskRange = [sentence rangeOfString:@"*" options:NSBackwardsSearch];
+
+    if (asteriskRange.location == NSNotFound) {
+        return YES;
+    }
+    else {
+        NSRange checksumRange = NSMakeRange(asteriskRange.location + 1, 2);
+        NSString *checksumString = [sentence substringWithRange:checksumRange];
+        NSUInteger checksum = (NSUInteger) strtoull([checksumString UTF8String], NULL, 16);
+
+        NSUInteger actualChecksum = 0;
+        for (int i = 1; i < asteriskRange.location; i++) {
+            actualChecksum ^= [sentence characterAtIndex:i];
+        }
+        return checksum == actualChecksum;
+    }
 }
 
 - (instancetype)initWithFields:(NSArray *)fields {
--- FGNmeaKitTests/FGNmeaSentenceTests.m
+++ FGNmeaKitTests/FGNmeaSentenceTests.m
@@ -10,6 +10,13 @@
 
 @end
 
+// Category to make private methods visible
+@interface FGNmeaSentence (Test)
+
++ (BOOL)validateChecksum:(NSString *)sentence;
+
+@end
+
 @implementation FGNmeaSentenceTests
 
 - (void)testNilStringCreatesNilSentence {
@@ -23,4 +30,34 @@
     XCTAssertNil(sentence, @"Expected nil result for nil-sentence");
 }
 
+- (void)testErrorExpectedForSentenceWithInvalidChecksum {
+    NSError *error;
+    FGNmeaSentence *sentence = [FGNmeaSentence nmeaSentenceFromString:@"$GPGSA,A,1,,,,,,,,,,,,,0.0,0.0,0.0*AA" error:&error];
+    XCTAssertNotNil(error, @"Expected error object for sentence with invalid checksum");
+    XCTAssertNil(sentence, @"Expected nil result for nil-sentence");
+}
+
+- (void)testValidateChecksum {
+
+    // Valid - no checksum
+    NSString *sentence = @"$GPRMC,081836,A,3751.65,S,14507.36,E,000.0,360.0,130998,011.3,E";
+    XCTAssertTrue([FGNmeaSentence validateChecksum:sentence], @"expected YES due to no checksum available");
+
+    // Valid
+    sentence = @"$GPRMC,081836,A,3751.65,S,14507.36,E,000.0,360.0,130998,011.3,E*62";
+    XCTAssertTrue([FGNmeaSentence validateChecksum:sentence], @"expected correct checksum");
+
+    sentence = @"$GPGSA,A,1,,,,,,,,,,,,,0.0,0.0,0.0*30";
+    XCTAssertTrue([FGNmeaSentence validateChecksum:sentence], @"expected correct checksum");
+
+    sentence = @"$GPGSV,3,1,09,3,,,,6,73,209,28,10,,,,13,,,*41";
+    XCTAssertTrue([FGNmeaSentence validateChecksum:sentence], @"expected correct checksum");
+
+    // Invalid
+    sentence = @"$GPGSA,A,1,,,,,,,,,,,,,0.0,0.0,0.0*AA";
+    XCTAssertFalse([FGNmeaSentence validateChecksum:sentence], @"expected invalid checksum");
+
+    sentence = @"$GPGSV,3,1,09,3,,,,6,73,209,28,10,,,,13,,,*0C";
+    XCTAssertFalse([FGNmeaSentence validateChecksum:sentence], @"expected invalid checksum");
+}
 @end
