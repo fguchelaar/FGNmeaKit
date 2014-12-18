8757b2c0e6166314463de30fd26682b5a86c9a42
fguchelaar <fguchelaar@gmail.com>
2014-12-18 14:29:45 +0100

    added GPRMC specific fields. Concrete types are yet to be determined

--- FGNmeaKit/Sentences/FGNmeaSentence_GPRMC.h
+++ FGNmeaKit/Sentences/FGNmeaSentence_GPRMC.h
@@ -15,4 +15,57 @@
 */
 @interface FGNmeaSentence_GPRMC : FGNmeaSentence
 
+/**
+* UTC of position fix
+*
+* field 1
+*/
+@property(nonatomic, strong) id time;
+
+/**
+* field 2
+*/
+@property(nonatomic, strong) id status;
+
+/**
+* field 3+4
+*/
+@property(nonatomic, strong) id latitude;
+
+/**
+* field 5+6
+*/
+@property(nonatomic, strong) id longitude;
+
+/**
+* Speed over ground, knots
+*
+* field 7
+*/
+@property(nonatomic, strong) id speedOverGround;
+
+/**
+* Course over ground, degrees True
+*
+* field 8
+*/
+@property(nonatomic, strong) id courseOverGround;
+
+/**
+* field 9
+*/
+@property(nonatomic, strong) id date;
+
+/**
+* Magnetic variation, degrees E/W
+*
+* field 10 + 11
+*/
+@property(nonatomic, strong) id magneticVariation;
+
+/**
+* field 12
+*/
+@property(nonatomic, strong) id modeIndicator;
+
 @end
