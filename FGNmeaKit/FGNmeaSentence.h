212cdd32f891ab66803545674b3012fa16e2acf4
fguchelaar <fguchelaar@gmail.com>
2014-12-18 20:19:48 +0100

    Added documentation

--- FGNmeaKit/FGNmeaSentence.h
+++ FGNmeaKit/FGNmeaSentence.h
@@ -11,12 +11,31 @@
 */
 @interface FGNmeaSentence : NSObject
 
+/**
+* Alphanumeric characters identifying type of TALKER. and Sentence Formatter.
+*/
 @property(nonatomic, copy) NSString *address;
 
+/**
+* Returns the first 2 characters of the address field.
+*/
 @property(nonatomic, readonly) NSString *talkerId;
 
+/**
+* Return the third till fifth characters of the address field.
+*/
 @property(nonatomic, readonly) NSString *sentenceFormatter;
 
+/**
+* After validating the sentence, an instance of an FGNmeaSentence subclass will be returned. We try to instantiate a
+* class by using the address of the sentence; ie GPRMC -> FGNmeaSentence_GPRMC.
+*/
 + (FGNmeaSentence *)nmeaSentenceFromString:(NSString *)sentence error:(NSError **)error;
 
+/**
+* Subclasses should implement this method and:
+* 1. validate the input and set error when appropriate
+* 2. set corresponding properties
+*/
+- (void)interpretFields:(NSArray *)array error:(NSError **)error;
 @end
